c68306fedf034b140387db913787ce45395ac713
neword <newordn@gmail.com>
2020-04-14 21:10:11 +0100

    First commit : Added the schema and the prisma model

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Class",
+    embedded: false
+  },
+  {
+    name: "Course",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/neword-2bedf8/MaApi/dev`
+});
+exports.prisma = new exports.Prisma();
