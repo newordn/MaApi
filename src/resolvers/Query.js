a34f92bffc2185618dfe51e96be3091f89c4d874
neword <newordn@gmail.com>
2020-04-18 18:04:16 +0100

    Added some logs

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,16 +1,19 @@
 const info = ()=> `Welcome on Ma Api`
 const classes = async (parent,args,context,info)=>
 {
+    console.log('classes query')
     const classes = await context.prisma.classes({orderBy:'id_DESC'})
     return classes
 }
 const courses = async (parent,args,context,info)=>
 {
+    console.log('courses query')
     const courses = await context.prisma.courses({orderBy:'id_DESC'})
     return courses
 }
 const coursesByClass = async (parent,args,context,info)=>
 {
+    console.log('courses by class query')
     const courses = await context.prisma.class({id:args.classId}).courses({orderBy:'id_DESC'})
     return courses
 }
